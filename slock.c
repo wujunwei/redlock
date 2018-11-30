fd32f490971a55e974f82e8f14f0337a8b72847c
adam <adam.wu@shein.com>
2018-11-30 14:38:48 +0800

    add lock info

--- slock.c
+++ slock.c
@@ -7,15 +7,9 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdint.h>
-#include <sys/time.h>
 
-//#ifndef SUCCESS
-//#define SUCCESS 1
-//#endif
-//
-//#ifndef FAIL
-//#define FAIL 0
-//#endif
+
+
 
 static RedisModuleType *SLockType;
 
@@ -23,6 +17,7 @@ struct SLock {
     mstime_t lock_time;
     unsigned long long client_id;
 };
+
 /**
  *  slock.lock lock_key expire
  *  the comand lock will return result immediately ,
@@ -34,29 +29,23 @@ struct SLock {
  */
 int SlockLock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     RedisModule_AutoMemory(ctx);
-
     if (argc < 2){
         return RedisModule_WrongArity(ctx);
     }
-    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[2], REDISMODULE_READ|REDISMODULE_WRITE);
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ|REDISMODULE_WRITE);
     int keyType = RedisModule_KeyType(key);
     if(keyType == REDISMODULE_KEYTYPE_EMPTY){
         struct SLock *lock = RedisModule_Alloc(sizeof(*lock));
         lock->client_id = RedisModule_GetClientId(ctx);
-        //get system microsecond
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        long long ust = ((long long)tv.tv_sec)*1000000;
-        ust += tv.tv_usec;
-        lock->lock_time = ust/1000;
+        lock->lock_time = RedisModule_Milliseconds();
 
         RedisModule_ModuleTypeSetValue(key, SLockType, lock);
         if(argc == 3){
-            long long more_time;
-            if (RedisModule_StringToLongLong(argv[2], &more_time) != REDISMODULE_OK){
+            long long expire_time;
+            if (RedisModule_StringToLongLong(argv[2], &expire_time) != REDISMODULE_OK){
                 return RedisModule_ReplyWithError(ctx,"ERR invalid expireTime");
             }
-            RedisModule_SetExpire(key, lock->lock_time + more_time);
+            RedisModule_SetExpire(key, expire_time);
         }
         RedisModule_ReplyWithLongLong(ctx,1);
     } else{
@@ -65,6 +54,36 @@ int SlockLock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
         }
         RedisModule_ReplyWithLongLong(ctx,0);
     }
+    RedisModule_CloseKey(key);
+    return REDISMODULE_OK;
+}
+
+
+/**
+ * SLOCK.INFO  key
+ *  this command will return an array with two var, first is the client id and second is the time when client require the lock.
+ * @param ctx
+ * @param argv
+ * @param agrc
+ * @return int
+ */
+int SlockInfo_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    if (argc != 2){
+        return RedisModule_WrongArity(ctx);
+    }
+
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ);
+    if (RedisModule_ModuleTypeGetType(key) != SLockType){ //判断键类型
+        return RedisModule_ReplyWithError(ctx,REDISMODULE_ERRORMSG_WRONGTYPE);
+    } else{
+        struct SLock *lock = RedisModule_ModuleTypeGetValue(key);
+        RedisModule_ReplyWithArray(ctx, 2);
+        RedisModule_ReplyWithLongLong(ctx, lock->client_id);
+        RedisModule_ReplyWithLongLong(ctx, lock->lock_time);
+        RedisModule_Free(lock);
+    }
+    RedisModule_CloseKey(key);
     return REDISMODULE_OK;
 }
 
@@ -73,7 +92,7 @@ int SlockLock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
  * @param ctx
  * @param argv
  * @param argc
- * @return
+ * @return int
  */
 int SlockunLock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 {
@@ -94,6 +113,7 @@ int SlockunLock_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int 
             RedisModule_ReplyWithLongLong(ctx, 0);
         }
     }
+    RedisModule_CloseKey(key);
     return REDISMODULE_OK;
 }
 
@@ -160,14 +180,14 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     SLockType = RedisModule_CreateDataType(ctx,"slocktype",0,&tm);
     if (SLockType == NULL) return REDISMODULE_ERR;
 
-    if (RedisModule_CreateCommand(ctx,"slock.lock",
-                                  SlockLock_RedisCommand,"write deny-oom",1,1,1) == REDISMODULE_ERR)
+    if (RedisModule_CreateCommand(ctx,"slock.unlock", SlockunLock_RedisCommand,"write",1,1,1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
-    if (RedisModule_CreateCommand(ctx,"slock.unlock",
-                                  SlockunLock_RedisCommand,"write",1,1,1) == REDISMODULE_ERR)
+    if (RedisModule_CreateCommand(ctx,"slock.lock", SlockLock_RedisCommand,"write",1,1,1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx,"slock.info", SlockInfo_RedisCommand,"readonly",1,1,1) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
 
     return REDISMODULE_OK;
 }
