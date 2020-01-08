4c678a5ae78bcd8f0e09de212bc0d7cc8af73ab4
adam <wjw3323@live.com>
2020-01-08 22:37:18 +0800

    change php test case

--- tests/client1.php
+++ tests/client1.php
@@ -6,5 +6,5 @@ $a->connect("127.0.0.1", "6379");
 //try lock,it will fail
 var_dump($a->rawCommand("slock.lock", ["test", 10000]));
 //try unlock it will fail too when the lock was locked by client.php
-var_dump($a->rawCommand("slock.unlock", ["test", 10000]));
+var_dump($a->rawCommand("slock.unlock", "test"));
 $a->close();
